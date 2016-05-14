55dbd01b054193259a43982b04a191d738d7c2dd
wonyoung <wonyoungjjang@gmail.com>
2016-05-14 18:25:09 +0900

    added socket.io

--- app/server.js
+++ app/server.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
-
-var app = express();
+var app  = require('express')();
+var hbs  = require('express-handlebars');
+var http = require('http').Server(app);
+var io   = require('socket.io')(http);
 
 app.set('views', 'app/views');
-app.engine('hbs', exphbs({
+app.engine('hbs', hbs({
   defaultLayout: 'main',
   extname: '.hbs',
   layoutsDir: "app/views/layouts/"
@@ -15,4 +15,13 @@ app.get('/', function(req, res) {
   res.render('home');
 });
 
-app.listen(3000);
+io.on('connection', function(socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, function() {
+  console.log('Listening on port 3000.');
+});
